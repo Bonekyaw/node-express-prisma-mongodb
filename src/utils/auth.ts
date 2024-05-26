034d0e3e1e6eb49cba8283e5719a5549c1959b94
bonekyaw <phonenai2014@gmail.com>
2024-05-26 14:55:36 +0630

    Added file upload

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -25,7 +25,17 @@ export const checkOtpPhone = (otpCheck: any) => {
 
 export const checkOtpErrorIfSameDate = (isSameDate: boolean, otpCheck: any) => {
   if (isSameDate && otpCheck.error === 5) {
-    const err: any = new Error("OTP is wrong 5 times today. Try again tomorrow.");
+    const err: any = new Error(
+      "OTP is wrong 5 times today. Try again tomorrow."
+    );
+    err.status = 401;
+    throw err;
+  }
+};
+
+export const checkAdmin = (admin: any) => {
+  if (!admin) {
+    const err: any = new Error("This account has not registered!.");
     err.status = 401;
     throw err;
   }
